363a2854c4b011a3d413e4e0f2e0d0c6b1b10b2b
Gokhan Dedeoglu <gokhandedeoglu@users.noreply.github.com>
2020-05-11 17:07:03 +0300

    Updated assignment1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,16 @@
 def players_as_dictionaries(squads_list):
-    pass
+    final_list=[]
+    for i in squads_list:
+        temp_dict= {
+            'number': i[0],
+            'position': i[1],
+            'name': i[2],
+            'date_of_birth': i[3],
+            'caps': i[4],
+            'club': i[5],
+            'country': i[6],
+            'club_country': i[7],
+            'year': i[8]
+            }
+        final_list.append(temp_dict)
+    return final_list
